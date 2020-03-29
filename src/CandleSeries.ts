c7eb47bab4d3142d90c6fe45f41944ee2effc69d
Pekka Maanpää <pekkamaa@vaadin.com>
2020-03-29 19:12:03 +0300

    Add TimeTraveller API to iterate over CandleSeries
    which can be used in the backtesting system.

--- src/CandleSeries.ts
+++ src/CandleSeries.ts
@@ -25,6 +25,18 @@ export class CandleSeries extends Array<Candle> {
   }
 
   /**
+   * Returns a TimeTraveller for this series, that can be
+   * used to simulate how the data is received one candle
+   * at a time.
+   * 
+   * @param startTime the unix time of the last candle to
+   * be included in the first iteration
+   */
+  getTimeTraveller(startTime: number): TimeTraveller {
+    return new TimeTraveller(this, startTime);
+  }
+
+  /**
    * Gets the time range of the candles as unix timestamps.
    */
   get range(): { start: number, end: number } {
@@ -43,6 +55,54 @@ export class CandleSeries extends Array<Candle> {
       start: this[0].time,
       end: this[this.length - 1].time
     };
+  }
+
+}
+
+/**
+ * Allows iterating over a CandleSeries, by expanding a
+ * subseries with one more candle, each time when calling
+ * the next() function.
+ * 
+ * This can be used by the backtesting system, to simulate
+ * going through the history one candle at a time.
+ */
+export class TimeTraveller {
+
+  private readonly series: CandleSeries;
+  /**
+   * Index of the candle that will be the last one included
+   * in the next call of next() function.
+   */
+  private nextIndex: number;
+
+  constructor(series: CandleSeries, startTime: number) {
+    this.series = series;
+    this.nextIndex = series.findIndex(candle =>
+      candle.time.getTime() === startTime);
+    if (this.nextIndex < 0) {
+      throw new Error('Failed to create TimeTraveller. ' +
+        'Could not find a candle with startTime ' + startTime);
+    }
+  }
+
+  /**
+   * Returns the next subseries, including one more candle
+   * compared to the previous call of this function.
+   */
+  next(): CandleSeries {
+    if (!this.hasNext()) {
+      throw new Error('TimeTraveller index out of bounds.');
+    }
+    return new CandleSeries(...this.series.slice(0, ++this.nextIndex));
+  }
+
+  /**
+   * Returns true if the iteration has not finished, and the
+   * next() function can still be called succesfully.
+   */
+  hasNext(): boolean {
+    return this.nextIndex >= 0 && this.nextIndex < this.series.length;
   }
 
 }
