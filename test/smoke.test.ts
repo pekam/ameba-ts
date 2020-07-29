3dfa76f7b86c52574b9a84326c9befe45f391a18
Pekka Maanpää <pekkamaa@vaadin.com>
2020-07-29 19:19:48 +0300

    Add smoke test

---
+++ test/smoke.test.ts
@@ -0,0 +1,55 @@
+import { loadCandles } from "../src/loadData";
+import { Strategy, Order, Trade } from "../src/types";
+import { backtestStrategy } from "../src/backtest";
+import {CandleSeries} from "../src/CandleSeries";
+
+it('should get end balance from backtest', async () => {
+
+  expect.assertions(1);
+
+  const strat: Strategy = state => {
+    const newCandle = state.series.last;
+    if (!state.position) {
+      if (newCandle.close > newCandle.open) {
+        const entryOrder: Order = {
+          type: 'limit',
+          price: newCandle.high
+        }
+        return {
+          entryOrder,
+          stopLoss: newCandle.high * 0.9999,
+          takeProfit: newCandle.high * 1.0001
+        }
+      } else {
+        return {
+          entryOrder: null,
+          stopLoss: null,
+          takeProfit: null
+        }
+      }
+    } else {
+      return {};
+    }
+  }
+
+  const series: CandleSeries = await loadCandles({
+    market: 'forex',
+    symbol: 'OANDA:EUR_USD',
+    resolution: '1',
+    from: Date.UTC(2020, 2, 4),
+    to: Date.UTC(2020, 2, 5)
+  });
+
+  const result: Trade[] =
+      backtestStrategy(strat, series,
+          Date.UTC(2020, 2, 4, 5),
+          Date.UTC(2020, 2, 4, 6));
+
+  let balance = 1000;
+  result.forEach(trade => {
+    balance *= (1 + trade.profit);
+  });
+
+  expect(balance).toBe(999.600040003999);
+
+});
