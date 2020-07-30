553d2df14bd968ddaac0e6202ca0a254e75b0f6f
Pekka Maanpää <pekkamaa@vaadin.com>
2020-07-30 19:06:55 +0300

    Add util functions to find all highs and lows

---
+++ src/strats/series-util.ts
@@ -0,0 +1,66 @@
+import { CandleSeries } from "../candle-series";
+
+/**
+ * Returns the indices of candles which have a local maximum.
+ */
+export function findHighIndices(series: CandleSeries): number[] {
+  return filterIndices(
+    series.map((candle) => candle.high),
+    isLocalMax
+  );
+}
+
+/**
+ * Returns the indices of candles which have a local minimum.
+ */
+export function findLowIndices(series: CandleSeries): number[] {
+  return filterIndices(
+    series.map((candle) => candle.low),
+    isLocalMin
+  );
+}
+
+function filterIndices(
+  series: number[],
+  filter: (series: number[], index: number) => boolean
+) {
+  return series.reduce((acc, current, index) => {
+    if (filter(series, index)) {
+      return acc.concat(index);
+    }
+    return acc;
+  }, []);
+}
+
+function isLocalMax(series: number[], index: number): boolean {
+  return compareToNeighbours(
+    series,
+    index,
+    (current, neighbour) => current > neighbour
+  );
+}
+
+function isLocalMin(series: number[], index: number): boolean {
+  return compareToNeighbours(
+    series,
+    index,
+    (current, neighbour) => current < neighbour
+  );
+}
+
+function compareToNeighbours(
+  series: number[],
+  index: number,
+  comparator: (current: number, neighbour: number) => boolean
+): boolean {
+  const current = series[index];
+  const previous = series[index - 1];
+  const next = series[index + 1];
+
+  return (
+    previous !== undefined &&
+    next !== undefined &&
+    comparator(current, previous) &&
+    comparator(current, next)
+  );
+}
---
+++ test/series-util.test.ts
@@ -0,0 +1,21 @@
+import { initTestData } from "./testData";
+import { CandleSeries } from "../src/candle-series";
+import { findHighIndices, findLowIndices } from "../src/strats/series-util";
+
+it("should find highs (local maximums)", () => {
+  const series: CandleSeries = initTestData();
+
+  const highIndices = findHighIndices(series);
+  const expectedHighIndices = [3, 7, 17, 19];
+
+  expect(highIndices).toEqual(expectedHighIndices);
+});
+
+it("should find lows (local minimums)", () => {
+  const series: CandleSeries = initTestData();
+
+  const lowIndices = findLowIndices(series);
+  const expectedLowIndices = [4, 9, 20];
+
+  expect(lowIndices).toEqual(expectedLowIndices);
+});
