cc6bd50a23633614b165f8836761c1aa4667c2ea
Pekka Maanpää <pekkamaa@vaadin.com>
2020-09-08 19:08:27 +0300

    Use SMA instance and .nextValue in DonchianChannelStrat

--- src/strats/donchian-channel-strat.ts
+++ src/strats/donchian-channel-strat.ts
@@ -1,6 +1,7 @@
 import { Order, Strategy, TradeState } from "../core/types";
 import { CandleSeries } from "../core/candle-series";
 import { getAverageCandleSize } from "./series-util";
+import { SMA } from "technicalindicators";
 
 const channelPeriod = 30;
 
@@ -10,14 +11,23 @@ const channelPeriod = 30;
  * Sell when crossing SMA 20.
  */
 export class DonchianChannelStrategy implements Strategy {
-  init(tradeState: TradeState): void {}
+  private sma: SMA;
+
+  init(state: TradeState): void {
+    this.sma = new SMA({
+      period: 20,
+      values: state.series
+        .slice(0, state.series.length - 1)
+        .map((c) => c.close),
+    });
+  }
 
   update(
     state: TradeState
   ): { entryOrder?: Order; stopLoss?: number; takeProfit?: number } {
     const series = state.series;
 
-    const sma = series.last.indicators.sma(20);
+    const sma = this.sma.nextValue(state.series.last.close);
 
     if (series.length < channelPeriod) {
       return {};
