6ce04f40ee05626fa7e8bd064fd56bd507f93ff6
Pekka Maanpää <pekkamaa@vaadin.com>
2020-10-11 21:18:49 +0300

    Init webapp with express and create-react-app
    Some redundant stuff removed from the react app.
    Jest version downgraded in the root module to not conflict with react app.
    
    Based on this tutorial:
    https://medium.com/free-code-camp/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0

---
+++ webapp/client/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  fetch("api/hello")
+    .then((r) => r.text())
+    .then(console.log);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
