31ed07e4fc11e179f7fd78f3a7043a94effc8cb5
Pekka Maanpää <pekkamaa@vaadin.com>
2020-10-16 11:40:42 +0300

    Parse dataSet and symbols from query params
    for chart rendering in the webapp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Chart from "./Chart";
 
 function App() {
+  const [dataSet, setDataSet] = useState<string | null>(null);
+  const [symbols, setSymbols] = useState<string[]>([]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const dataSet = urlParams.get("dataSet");
+    const symbols = urlParams.get("symbols");
+    if (dataSet && symbols) {
+      setDataSet(dataSet);
+      setSymbols(JSON.parse(symbols));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Chart dataSet="makkara" symbol="TU" />
+      {dataSet &&
+        symbols.map((s) => <Chart dataSet={dataSet} symbol={s} key={s} />)}
     </div>
   );
 }
