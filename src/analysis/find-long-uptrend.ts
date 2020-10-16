e4f5bd311e4999d41b0854b1a1368ddb77361cf5
Pekka Maanpää <pekkamaa@vaadin.com>
2020-10-16 20:15:49 +0300

    Add scoring function for long uptrend

---
+++ src/analysis/find-long-uptrend.ts
@@ -0,0 +1,30 @@
+import { CandleSeries } from "../core/candle-series";
+import { rankAndReport } from "./rank-and-report";
+
+/**
+ * Scores the candle series by the number of consecutive candles
+ * that are above the simple moving average, taken from the end
+ * of the series.
+ */
+const scoreByLongTrend = (series: CandleSeries) => {
+  const smaPeriod = 20;
+
+  let index = series.length - 1;
+  let counter = 0;
+  while (index > smaPeriod) {
+    const sma =
+      series
+        .slice(index - smaPeriod, index)
+        .map((c) => c.close)
+        .reduce((a, b) => a + b) / smaPeriod;
+    if (series[index].low < sma) {
+      break;
+    }
+    counter++;
+    index--;
+  }
+
+  return counter;
+};
+
+rankAndReport("makkara", scoreByLongTrend);
