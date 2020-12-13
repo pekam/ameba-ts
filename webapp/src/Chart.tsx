bcf1141cbdfa736032c7852812946b5f1e53557b
Pekka Maanpää <pekkamaa@vaadin.com>
2020-12-13 15:23:56 +0200

    Support marker below bar in webapp charts

--- webapp/src/Chart.tsx
+++ webapp/src/Chart.tsx
@@ -7,7 +7,9 @@ import "./Chart.css";
  * @param dataSet the name of the data set from which to load the candles
  * @param symbolAndCandleTimes the symbol whose candles to load, and
  * optional timestamps of candles to mark in the chart separated by underscores,
- * e.g. "AAPL_123000000_124000000"
+ * e.g. "AAPL_123000000_124000000". The marker will be above the candle,
+ * unless its prefixed with "b", in which case the marker will be
+ * below the candle, e.g. "AAPL_b123000000".
  */
 function Chart({
   dataSet,
@@ -18,7 +20,7 @@ function Chart({
 }) {
   const chartInput = symbolAndCandleTimes.split("_");
   const symbol = chartInput[0];
-  const candleTimes = chartInput.slice(1).map((time) => parseInt(time));
+  const candleTimes = chartInput.slice(1);
 
   const chartId = `${dataSet}-${symbol}`;
   const [company, setCompany] = useState<CompanyWithCandles | null>(null);
@@ -35,13 +37,19 @@ function Chart({
         });
         const series = chart.addCandlestickSeries();
 
-        const markers = candleTimes.map((candleTime) => ({
-          time: candleTime,
-          position: "aboveBar",
-          color: "#2196F3",
-          shape: "arrowDown",
-          size: 0.5,
-        }));
+        const markers = candleTimes.map((candleTime) => {
+          const markerBelow = candleTime.startsWith("b");
+          const time = parseInt(
+            markerBelow ? candleTime.substr(1) : candleTime
+          );
+          return {
+            time: time,
+            position: markerBelow ? "belowBar" : "aboveBar",
+            color: "#2196F3",
+            shape: markerBelow ? "arrowUp" : "arrowDown",
+            size: 0.5,
+          };
+        });
         // @ts-ignore
         series.setMarkers(markers);
 
