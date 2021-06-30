faddb49cee81c2f31dd271685c0588ddcf6f2068
Pekka Maanpää <pekkamaa@vaadin.com>
2021-06-30 15:20:24 +0300

    Add macd strategy

---
+++ src/strats/macd-strat.ts
@@ -0,0 +1,112 @@
+import {
+  MarketPosition,
+  Order,
+  Strategy,
+  StrategyUpdate,
+  TradeState,
+} from "../core/types";
+import { Indicators } from "../core/indicators";
+import { m } from "../functions/functions";
+
+export class MacdStrat implements Strategy {
+  private indicators: Indicators;
+
+  constructor(
+    private settings: {
+      relativeTakeProfit;
+      relativeStopLoss;
+      onlyDirection?: MarketPosition;
+    }
+  ) {}
+
+  init(state: TradeState): void {
+    this.indicators = new Indicators(
+      {
+        macdSettings: { fastPeriod: 12, slowPeriod: 26, signalPeriod: 9 },
+      },
+      state.series
+    );
+  }
+  update: StrategyUpdate = (state) => {
+    const series = state.series;
+    const candle = m.last(series);
+
+    const { macd } = this.indicators.update(series);
+
+    const previousMacd = (() => {
+      const prevIndicators = this.indicators.get(m.get(series, -2));
+      return prevIndicators && prevIndicators.macd;
+    })();
+
+    if (!macd || !previousMacd) {
+      return {};
+    }
+
+    if (state.position) {
+      return {};
+    }
+
+    // All must be true to go long, all must be false to go short
+    const bullishFilters = [
+      macd.macd > 0,
+      previousMacd.histogram < 0,
+      previousMacd.histogram < macd.histogram,
+    ];
+
+    if (
+      this.settings.onlyDirection !== "short" &&
+      bullishFilters.every((b) => b)
+    ) {
+      // Long
+      const entryOrder: Order = {
+        side: "buy",
+        type: "stop",
+        price: candle.high,
+      };
+      const long = {
+        entryOrder,
+        ...relativeExits({ entryOrder, ...this.settings }),
+      };
+      return long;
+    }
+    if (
+      this.settings.onlyDirection !== "long" &&
+      bullishFilters.every((b) => !b)
+    ) {
+      // Short
+      const entryOrder: Order = {
+        side: "sell",
+        type: "stop",
+        price: candle.low,
+      };
+      const short = {
+        entryOrder,
+        ...relativeExits({ entryOrder, ...this.settings }),
+      };
+      return short;
+    }
+    return { entryOrder: null };
+  };
+}
+
+function relativeExits({
+  entryOrder,
+  relativeTakeProfit,
+  relativeStopLoss,
+}: {
+  entryOrder: Order;
+  relativeTakeProfit: number;
+  relativeStopLoss: number;
+}): { takeProfit: number; stopLoss: number } {
+  if (entryOrder.side === "buy") {
+    return {
+      takeProfit: entryOrder.price * (1 + relativeTakeProfit),
+      stopLoss: entryOrder.price * (1 - relativeStopLoss),
+    };
+  } else {
+    return {
+      takeProfit: entryOrder.price * (1 - relativeTakeProfit),
+      stopLoss: entryOrder.price * (1 + relativeStopLoss),
+    };
+  }
+}
