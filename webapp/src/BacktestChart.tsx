d1682dd6e54356f258cf8176a6d04dd008a83c30
Pekka Maanpää <pekkamaa@vaadin.com>
2021-07-24 16:01:47 +0300

    Webapp: Mark trades in backtest chart
    with arrows and area charts

--- webapp/src/BacktestChart.tsx
+++ webapp/src/BacktestChart.tsx
@@ -1,4 +1,4 @@
-import { createChart, CrosshairMode } from "lightweight-charts";
+import { createChart, CrosshairMode, LineStyle } from "lightweight-charts";
 import React, { useEffect } from "react";
 import { CandleSeries } from "../../src/core/types";
 import { FtxBacktestResult } from "../../src/ftx/ftx-backtest-store";
@@ -21,10 +21,51 @@ function BacktestChart({
       crosshair: { mode: CrosshairMode.Normal },
     });
 
-    const series = chart.addCandlestickSeries();
+    const series = chart.addCandlestickSeries({
+      priceLineVisible: false,
+      lastValueVisible: false,
+    });
+
+    function formatProfit(profit: number): string {
+      return `${profit > 0 ? "+" : ""}${(profit * 100).toFixed(2)}%`;
+    }
+
+    const markers = ftxBacktestResult.result.trades.flatMap((trade) => {
+      return [trade.entry, trade.exit].map((order, i) => ({
+        time: order.time,
+        position: order.side === "buy" ? "belowBar" : "aboveBar",
+        shape: order.side === "buy" ? "arrowUp" : "arrowDown",
+        color: "#2196F3",
+        size: 0.5,
+        text: i === 1 ? formatProfit(trade.profit) : undefined,
+      }));
+    });
+
+    // @ts-ignore
+    series.setMarkers(markers);
     // @ts-ignore
     series.setData(candles);
-    series.applyOptions({ priceLineVisible: false, lastValueVisible: false });
+
+    ftxBacktestResult.result.trades.forEach((trade) => {
+      const color = trade.profit > 0 ? "lightgreen" : "pink";
+      const area = chart.addAreaSeries({
+        priceLineVisible: false,
+        lastValueVisible: false,
+        crosshairMarkerVisible: false,
+        lineStyle: LineStyle.Solid,
+        lineWidth: 1,
+        lineColor: color,
+        topColor: color,
+        bottomColor: color,
+      });
+      area.setData(
+        // @ts-ignore
+        [trade.entry, trade.exit].map((order) => ({
+          time: order.time,
+          value: order.price,
+        }))
+      );
+    });
   });
 
   return <div id="backtestChart"></div>;
