c84774d89b9b4b4838ba54cca86da9b9ef5cb459
Pekka Maanpää <pekkamaa@vaadin.com>
2021-07-29 11:46:09 +0300

    Remove unused trailing-stop-loss

--- src/strats/trailing-stop-loss.ts
+++
@@ -1,8 +0,0 @@
-import { StrategyUpdate, TradeState } from "../core/types";
-import { m } from "../shared/functions";
-
-export function trailingStopLoss(state: TradeState): StrategyUpdate {
-  return {
-    stopLoss: Math.max(state.stopLoss || 0, m.last(state.series).high * 0.95),
-  };
-}
