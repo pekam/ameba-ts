4df6a2905c7e5bbe96dd1999547e49ab2ef82753
Pekka Maanpää <pekkamaa@vaadin.com>
2021-08-10 15:33:34 +0300

    Add nonIncreasingStopLoss() to strat-utils

--- src/strats/strat-util.ts
+++ src/strats/strat-util.ts
@@ -1,4 +1,5 @@
-import { Order, StrategyUpdate } from "../core/types";
+import { Order, StrategyUpdate, TradeState } from "../core/types";
+import { m } from "../shared/functions";
 
 export const cancelEntry: StrategyUpdate = {
   entryOrder: null,
@@ -26,4 +27,41 @@ export function withRelativeExits({
       stopLoss: entryOrder.price * (1 + relativeStopLoss),
     };
   }
+}
+
+export function nonIncresingStopLoss({
+  state,
+  stopLossValue,
+  stopLossType,
+}: {
+  state: TradeState;
+  stopLossValue: number;
+  stopLossType: "absolute" | "diff";
+}): { stopLoss: number } {
+  if (stopLossValue < 0) {
+    throw Error("Stop loss value should be positive.");
+  }
+  if (!state.position) {
+    throw Error("There should already be a position.");
+  }
+
+  const potentialStopLoss: number = (function () {
+    if (stopLossType === "absolute") {
+      return stopLossValue;
+    } else if (stopLossType === "diff") {
+      const currentPrice = m.last(state.series).close;
+      return state.position === "long"
+        ? currentPrice - stopLossValue
+        : currentPrice + stopLossValue;
+    }
+    throw Error("Unhandled stop loss type.");
+  })();
+
+  const limiterFunc = state.position === "long" ? Math.max : Math.min;
+
+  return {
+    stopLoss: state.stopLoss
+      ? limiterFunc(state.stopLoss, potentialStopLoss)
+      : potentialStopLoss,
+  };
 }
