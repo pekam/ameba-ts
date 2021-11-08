6eac5fe186073fe33bd56d73a8e51234ec96ee52
Pekka Maanpää <pekkamaa@vaadin.com>
2021-11-08 09:46:20 +0200

    Remove FTX bots from Git, add examples instead

--- src/ftx/bot-examples.ts
+++ src/ftx/bot-examples.ts
@@ -3,13 +3,76 @@ import { CandleSeries } from "../core/types";
 import { m } from "../shared/functions";
 import { PERIODS } from "../shared/time-util";
 import { donchianBreakoutStrategy } from "../strats/donchian-breakout-strat";
+import { runFtxBot } from "./bot";
 import { botB } from "./bot-b";
 import { FtxMarket, getFtxClient } from "./ftx";
+import { getFtxStaker } from "./ftx-staker";
 import { getFtxUtil } from "./ftx-util";
+import { getFtxMarketMaker } from "./market-maker-orders";
+import { emaStrat } from "./strats";
 
-(async function () {
-  const subaccount = "bot-4";
+/*
+Examples for running bots in FTX.
+
+The first example uses the simple bot where strategies simply return a boolean
+to indicate whether to be long or not, and the buys and sells are executed by
+keeping a best bid/ask in the orderbook and waiting for someone to take them
+(to take advantage of FTX's 0 trading fees for market maker orders).
+
+The second bot runs botB, which integrates with this project's Strategy type. It
+uses stop and limit orders as defined by the strategy, so some limit orders might
+be market maker (0 fees), but mostly you shouldn't count on that.
+
+properties.json must contain entries for these subaccounts, e.g.
+{
+  "ftx": [
+    {
+      "subaccount": "bot-2",
+      "api_key": "...",
+      "s": "...",
+      "peak": 3450
+    },
+    {
+      "subaccount": "bot-4",
+      "api_key": "...",
+      "s": "..."
+    }
+  ]
+}
+*/
+
+async function botExample() {
+  const subaccount = "bot-2";
   const market: FtxMarket = "FTT/USD";
+
+  const strat = emaStrat;
+
+  const { enter, exit } = getFtxMarketMaker({
+    subaccount,
+    market,
+    staker: getFtxStaker({
+      subaccount,
+      market,
+      stakeByPeakAccountValue: true,
+      stratIfStakingByBacktest: strat,
+    }),
+  });
+
+  await runFtxBot({
+    subaccount,
+    market,
+    strat,
+    resolution: "1min",
+    candleSeriesLookBack: PERIODS.hour * 18,
+    safeZoneMargin: 0.002,
+    enter,
+    exit,
+  });
+}
+
+async function botBExample() {
+  const subaccount = "bot-4";
+  const market: FtxMarket = "SHIB/USD";
 
   const backtestInterval = PERIODS.day / 2;
   const backtestPeriod = backtestInterval;
@@ -67,4 +130,4 @@ import { getFtxUtil } from "./ftx-util";
     requiredCandles: backtestPeriod / PERIODS.minute + PERIODS.hour,
     stakerProvider: getStaker,
   });
-})();
+}
