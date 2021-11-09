c99e1e6cecf3213d1ff01e16080d48705338dba2
Pekka Maanpää <pekkamaa@vaadin.com>
2021-11-09 15:15:13 +0200

    Add getCandles and barToCandle functions for Alpaca
    to better support this project's types

--- src/alpaca/alpaca.ts
+++ src/alpaca/alpaca.ts
@@ -1,5 +1,7 @@
-import { AlpacaClient, AlpacaStream } from "@master-chief/alpaca";
+import { AlpacaClient, AlpacaStream, Bar, GetBars } from "@master-chief/alpaca";
+import { Candle } from "../core/types";
 import { properties } from "../properties";
+import { Moment, toJSDate, toTimestamp } from "../shared/time-util";
 
 const credentials = {
   key: properties.alpaca_api_key,
@@ -19,7 +21,45 @@ function getStream() {
   });
 }
 
+export type AlpacaResolution = GetBars["timeframe"];
+
+async function getCandles({
+  symbol,
+  from,
+  to,
+  resolution,
+}: {
+  symbol: string;
+  from: Moment;
+  to: Moment;
+  resolution: AlpacaResolution;
+}) {
+  const response = await alpaca.client.getBars({
+    start: toJSDate(from),
+    end: toJSDate(to),
+    symbol,
+    timeframe: resolution,
+  });
+  if (response.next_page_token) {
+    throw Error("Paging support not implemented yet.");
+  }
+  return response.bars.map(barToCandle);
+}
+
+function barToCandle(bar: Bar): Candle {
+  return {
+    open: bar.o,
+    high: bar.h,
+    low: bar.l,
+    close: bar.c,
+    volume: bar.v,
+    time: toTimestamp(bar.t),
+  };
+}
+
 export const alpaca = {
   client,
   getStream,
+  getCandles,
+  barToCandle,
 };
