5825485cf725eee6c72188fdaca77500db44285b
Pekka Maanpää <pekkamaa@vaadin.com>
2021-11-15 16:54:36 +0200

    Fix indicator tests
    which were not updated when the API changed

--- test/indicators.test.ts
+++ test/indicators.test.ts
@@ -1,18 +1,22 @@
 import { Indicators } from "../src/core/indicators";
-import { initTestData } from "./testData";
 import { CandleSeries } from "../src/core/types";
+import { initTestData } from "./testData";
 
 it("should calculate SMA", () => {
   const series: CandleSeries = Array.from(Array(5).keys()).map(
     (index: number) => {
-      return { close: index + 1, high: 0, low: 0, open: 0, time: 0, volume: 0 };
+      return {
+        close: index + 1,
+        high: 0,
+        low: 0,
+        open: 0,
+        time: index + 1,
+        volume: 0,
+      };
     }
   );
 
-  const indicators = new Indicators(
-    { smaPeriod: 3 },
-    series.slice(0, series.length - 1)
-  );
+  const indicators = new Indicators({ smaPeriod: 3 });
   expect(indicators.update(series.slice(0, series.length - 1)).sma).toEqual(3);
   expect(indicators.update(series.slice(0, series.length)).sma).toEqual(4);
 });
@@ -20,10 +24,7 @@ it("should calculate SMA", () => {
 it("should calculate RSI", () => {
   const series: CandleSeries = initTestData();
 
-  const indicators = new Indicators(
-    { rsiPeriod: 14 },
-    series.slice(0, series.length)
-  );
+  const indicators = new Indicators({ rsiPeriod: 14 });
 
   expect(indicators.update(series).rsi).toEqual(68.64);
 });
@@ -31,10 +32,7 @@ it("should calculate RSI", () => {
 it("should calculate ADX, -DI and +DI", () => {
   const series: CandleSeries = initTestData();
 
-  const indicators = new Indicators(
-    { adxPeriod: 5 },
-    series.slice(0, series.length)
-  );
+  const indicators = new Indicators({ adxPeriod: 5 });
 
   const indicatorValues = indicators.update(series.slice(0, series.length));
 
