398cf53fe8234243e84ffdbcecda7a984b387d02
Pekka Maanpää <pekkamaa@vaadin.com>
2022-01-07 21:44:05 +0200

    Refactor withStaker by splitting functions

--- src/high-level-api/with-staker.ts
+++ src/high-level-api/with-staker.ts
@@ -1,8 +1,13 @@
+import { pipe, reduce, toPairs } from "remeda";
 import {
+  AssetState,
   FullStrategyUpdate,
   FullTradeState,
   FullTradingStrategy,
+  Order,
+  SingleAssetStrategyUpdate,
 } from "../core/types";
+import { Dictionary } from "../util/type-util";
 import { Staker, StrategyUpdate, TradingStrategy } from "./types";
 
 /**
@@ -14,58 +19,75 @@ import { Staker, StrategyUpdate, TradingStrategy } from "./types";
  * @param strategy should be stateless, because the same strategy function may
  * be called for multiple assets
  */
-
-export function withStaker(
+export const withStaker = (
   strategy: TradingStrategy,
   staker: Staker
-): FullTradingStrategy {
-  return function (state: FullTradeState): FullStrategyUpdate {
-    const sizelessUpdates = state.updated
-      .map((symbol) => state.assets[symbol])
-      .reduce<{ [symbol: string]: StrategyUpdate }>((updates, asset) => {
-        const update = strategy(state.assets[asset.symbol]);
-        return { ...updates, [asset.symbol]: update };
-      }, {});
+): FullTradingStrategy => (state: FullTradeState): FullStrategyUpdate => {
+  const sizelessUpdates: Dictionary<StrategyUpdate> = pipe(
+    state,
+    getAssetsWithNewCandle,
+    getStrategyUpdates(strategy)
+  );
 
-    const stakes = staker(state, sizelessUpdates);
+  const stakes: Dictionary<number> = staker(state, sizelessUpdates);
 
-    return Object.entries(sizelessUpdates).reduce(
-      (sizedUpdates, [symbol, update]) => {
-        if (!update.entryOrder) {
-          return { ...sizedUpdates, [symbol]: update };
-        }
-        const size = stakes[symbol];
-        if (size === undefined) {
-          throw Error(
-            `Staker did not return an order size for an updated symbol ${symbol}.`
-          );
-        }
-        if (size < 0) {
-          throw Error(
-            `Staker returned a negative order size ${size} for symbol ${symbol}.`
-          );
-        }
-        if (size === 0) {
-          return {
-            ...sizedUpdates,
-            [symbol]: {
-              ...update,
-              entryOrder: null,
-            },
-          };
-        }
-        return {
-          ...sizedUpdates,
-          [symbol]: {
-            ...update,
-            entryOrder: {
-              ...update.entryOrder,
-              size,
-            },
-          },
-        };
-      },
-      {}
-    );
+  return addSizesToStrategyUpdates(sizelessUpdates, stakes);
+};
+
+const getAssetsWithNewCandle = (state: FullTradeState) =>
+  state.updated.map((symbol) => state.assets[symbol]);
+
+const getStrategyUpdates = (strategy: TradingStrategy) =>
+  reduce<AssetState, Dictionary<StrategyUpdate>>(
+    (updates, asset) => ({ ...updates, [asset.symbol]: strategy(asset) }),
+    {}
+  );
+
+const addSizesToStrategyUpdates = (
+  sizelessUpdates: Dictionary<StrategyUpdate>,
+  stakes: Dictionary<number>
+) => pipe(sizelessUpdates, toPairs, collectUpdatesWithSizes(stakes));
+
+const collectUpdatesWithSizes = (stakes: Dictionary<number>) =>
+  reduce<[string, StrategyUpdate], FullStrategyUpdate>(
+    (sizedUpdates, [symbol, update]) => ({
+      ...sizedUpdates,
+      [symbol]: addSizeToUpdate(symbol, update, stakes[symbol]),
+    }),
+    {}
+  );
+
+function addSizeToUpdate(
+  symbol: string,
+  update: StrategyUpdate,
+  size: number
+): SingleAssetStrategyUpdate {
+  if (!update.entryOrder) {
+    // TODO avoid this type assertion caused by TS language limitation
+    return update as SingleAssetStrategyUpdate;
+  }
+
+  validatePositionSize(size, symbol);
+
+  // Cancel the entry if size would be 0
+  const entryOrder: Order | null =
+    size === 0 ? null : { ...update.entryOrder, size };
+
+  return {
+    ...update,
+    entryOrder,
   };
+}
+
+function validatePositionSize(size: number | undefined, symbol: string) {
+  if (size === undefined) {
+    throw Error(
+      `Staker did not return an order size for an updated symbol ${symbol}.`
+    );
+  }
+  if (size < 0) {
+    throw Error(
+      `Staker returned a negative order size ${size} for symbol ${symbol}.`
+    );
+  }
 }
