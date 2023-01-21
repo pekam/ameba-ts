2f4574b7b48254298f9cce50d2f29f36174e68be
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-01-21 21:27:11 +0200

    Wrap ProgressHandler args in an object

--- src/core/progress-handler.ts
+++ src/core/progress-handler.ts
@@ -13,11 +13,11 @@ import { Presets, SingleBar } from "cli-progress";
  * provided by the backtest caller, and may be incorrect if the candle provider
  * stops sending candles before reaching 'to'.
  */
-export type ProgressHandler = (
-  currentTime: number,
-  startTime: number,
-  finishTime?: number
-) => void;
+export type ProgressHandler = (args: {
+  currentTime: number;
+  startTime: number;
+  finishTime?: number;
+}) => void;
 
 /**
  * Creates a backtest progress handler which renders a progress bar in the
@@ -25,7 +25,7 @@ export type ProgressHandler = (
  */
 export function createProgressBar(): ProgressHandler {
   let progressBar: SingleBar;
-  return (currentTime, startTime, finishTime) => {
+  return ({ currentTime, startTime, finishTime }) => {
     if (!finishTime) {
       return;
     }
