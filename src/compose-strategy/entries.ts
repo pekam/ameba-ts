128e87bc320a217ac3c222fcc11e0bb269b58b85
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-04-15 15:22:27 +0300

    Add more generic entry strategies, improve naming

--- src/compose-strategy/entries.ts
+++ src/compose-strategy/entries.ts
@@ -1,9 +1,11 @@
+import { OrderSide, OrderType } from "../core/types";
 import { Entry, STRATEGY_NOT_READY } from "./compose-strategy";
+import { ValueProvider } from "./filters";
 
 /**
  * Sets a market buy order to enter a long position.
  */
-export const marketBuy: Entry = () => ({
+export const marketBuyEntry: Entry = () => ({
   type: "market",
   side: "buy",
 });
@@ -11,41 +13,66 @@ export const marketBuy: Entry = () => ({
 /**
  * Sets a market sell order to enter a short position.
  */
-export const marketSell: Entry = () => ({
+export const marketSellEntry: Entry = () => ({
   type: "market",
   side: "sell",
 });
+
+const pricedEntry =
+  (type: Exclude<OrderType, "market">, side: OrderSide) =>
+  (orderPriceProvider: ValueProvider): Entry =>
+  (state) => {
+    const price = orderPriceProvider(state);
+    if (price === undefined) {
+      return STRATEGY_NOT_READY;
+    }
+    return {
+      type,
+      side,
+      price,
+    };
+  };
+
+/**
+ * An entry strategy that sets a limit buy order at a price provided by the
+ * given value provider.
+ */
+export const limitBuyEntry = pricedEntry("limit", "buy");
+/**
+ * An entry strategy that sets a limit sell order at a price provided by the
+ * given value provider.
+ */
+export const limitSellEntry = pricedEntry("limit", "sell");
+/**
+ * An entry strategy that sets a stop buy order at a price provided by the
+ * given value provider.
+ */
+export const stopBuyEntry = pricedEntry("stop", "buy");
+/**
+ * An entry strategy that sets a stop sell order at a price provided by the
+ * given value provider.
+ */
+export const stopSellEntry = pricedEntry("stop", "sell");
 
 /**
  * Sets a stop buy order to enter a long position when the price breaks above
  * last N candles.
  */
-export const breakoutBuy =
-  (period: number): Entry =>
-  ({ series }) => {
+export const breakoutBuyEntry = (period: number): Entry =>
+  stopBuyEntry(({ series }) => {
     if (series.length < period) {
-      return STRATEGY_NOT_READY;
+      return undefined;
     }
-    return {
-      side: "buy",
-      type: "stop",
-      price: Math.max(...series.slice(-period).map((c) => c.high)),
-    };
-  };
-
+    return Math.max(...series.slice(-period).map((c) => c.high));
+  });
 /**
  * Sets a stop sell order to enter a short position when the price breaks below
  * last N candles.
  */
-export const breakoutSell =
-  (period: number): Entry =>
-  ({ series }) => {
+export const breakoutSellEntry = (period: number): Entry =>
+  stopSellEntry(({ series }) => {
     if (series.length < period) {
-      return STRATEGY_NOT_READY;
+      return undefined;
     }
-    return {
-      side: "sell",
-      type: "stop",
-      price: Math.min(...series.slice(-period).map((c) => c.low)),
-    };
-  };
+    return Math.min(...series.slice(-period).map((c) => c.low));
+  });
