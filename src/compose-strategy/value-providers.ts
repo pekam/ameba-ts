64e69a70ce9c7f748ee0f82749f0299825488b59
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-04-15 15:57:04 +0300

    Add ValueProviders for a few more indicators

--- src/compose-strategy/value-providers.ts
+++ src/compose-strategy/value-providers.ts
@@ -1,14 +1,13 @@
 import { AssetState } from "../core/types";
-import { getAdx, getDonchianChannel, getSma } from "../indicators";
+import {
+  getAdx,
+  getAtr,
+  getDonchianChannel,
+  getEma,
+  getRsi,
+  getSma,
+} from "../indicators";
 import { ValueProvider } from "./types";
-
-/**
- * The simple moving average indicator as a {@link ValueProvider}.
- */
-export const sma =
-  (period: number, indexFromEnd?: number): ValueProvider =>
-  (state: AssetState) =>
-    getSma(state, period, indexFromEnd);
 
 /**
  * The average directional index indicator as a {@link ValueProvider}.
@@ -17,6 +16,38 @@ export const adx =
   (period: number, indexFromEnd?: number): ValueProvider =>
   (state: AssetState) =>
     getAdx(state, period, indexFromEnd)?.adx;
+
+/**
+ * The average true range indicator as a {@link ValueProvider}.
+ */
+export const atr =
+  (period: number, indexFromEnd?: number): ValueProvider =>
+  (state: AssetState) =>
+    getAtr(state, period, indexFromEnd);
+
+/**
+ * The exponential moving average indicator as a {@link ValueProvider}.
+ */
+export const ema =
+  (period: number, indexFromEnd?: number): ValueProvider =>
+  (state: AssetState) =>
+    getEma(state, period, indexFromEnd);
+
+/**
+ * The relative strength index indicator as a {@link ValueProvider}.
+ */
+export const rsi =
+  (period: number, indexFromEnd?: number): ValueProvider =>
+  (state: AssetState) =>
+    getRsi(state, period, indexFromEnd);
+
+/**
+ * The simple moving average indicator as a {@link ValueProvider}.
+ */
+export const sma =
+  (period: number, indexFromEnd?: number): ValueProvider =>
+  (state: AssetState) =>
+    getSma(state, period, indexFromEnd);
 
 /**
  * A {@link ValueProvider} that returns the max price reached in the last
